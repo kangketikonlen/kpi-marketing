90535d33f666bad68d8ba45dfe8de83cfcbdba34
Gilang Pratama <pratamapriadi96@gmail.com>
2024-04-29 13:00:43 +0700

    Add model & database.

---
+++ database/migrations/2024_04_29_124541_create_indicator_weights_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('indicator_weights', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('target');
+            $table->unsignedInteger('weight_percentage');
+            $table->integer('late_percentage');
+            $table->enum('type', ['Sales', 'Report']);
+            $table->enum('status', ['Aktif', 'Tidak Aktif']);
+            $table->string('createdBy')->default('System');
+            $table->timestamp('createdAt')->default(now());
+            $table->string('updatedBy')->nullable(true)->default(null);
+            $table->timestamp('updatedAt')->nullable(true)->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('indicator_weights');
+    }
+};
